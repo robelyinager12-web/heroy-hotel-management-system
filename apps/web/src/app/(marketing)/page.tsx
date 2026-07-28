cdd6800549f8df4f42bc993445c4d3deb1b9e3b1
robel <robelyinager@gmail.com>
2026-07-28 03:56:23 +0200

    assemble homepage with special offers and faq sections added

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -2,8 +2,10 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { HeroSection } from "@/components/home/HeroSection";
 import { RoomShowcase } from "@/components/home/RoomShowcase";
+import { SpecialOffers } from "@/components/home/SpecialOffers";
 import { FacilitiesSection } from "@/components/home/FacilitiesSection";
 import { TestimonialsSection } from "@/components/home/TestimonialsSection";
+import { FaqSection } from "@/components/home/FaqSection";
 
 export default function HomePage() {
   return (
@@ -11,8 +13,10 @@ export default function HomePage() {
       <Navbar />
       <HeroSection />
       <RoomShowcase />
+      <SpecialOffers />
       <FacilitiesSection />
       <TestimonialsSection />
+      <FaqSection />
       <Footer />
     </main>
   );
