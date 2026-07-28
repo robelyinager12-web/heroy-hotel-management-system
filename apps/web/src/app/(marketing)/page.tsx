5fec8c5950e543167a51163c2d6c70ca4123f252
robel <robelyinager@gmail.com>
2026-07-28 04:00:50 +0200

    assemble complete homepage with all marketing sections in order

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -3,8 +3,12 @@ import { Footer } from "@/components/layout/Footer";
 import { HeroSection } from "@/components/home/HeroSection";
 import { RoomShowcase } from "@/components/home/RoomShowcase";
 import { SpecialOffers } from "@/components/home/SpecialOffers";
+import { ServicesSection } from "@/components/home/ServicesSection";
 import { FacilitiesSection } from "@/components/home/FacilitiesSection";
+import { PricingSection } from "@/components/home/PricingSection";
 import { TestimonialsSection } from "@/components/home/TestimonialsSection";
+import { PartnersSection } from "@/components/home/PartnersSection";
+import { NewsSection } from "@/components/home/NewsSection";
 import { FaqSection } from "@/components/home/FaqSection";
 
 export default function HomePage() {
@@ -14,8 +18,12 @@ export default function HomePage() {
       <HeroSection />
       <RoomShowcase />
       <SpecialOffers />
+      <ServicesSection />
       <FacilitiesSection />
+      <PricingSection />
       <TestimonialsSection />
+      <PartnersSection />
+      <NewsSection />
       <FaqSection />
       <Footer />
     </main>
